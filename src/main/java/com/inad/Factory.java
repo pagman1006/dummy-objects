b99f62b2c270e4f9bf625886f120a9279465b119
Pedro Andres Gasca Manrique <pedroandres.gasca.contractor@bbva.com>
2025-12-01 23:01:23 -0600

    Instant, Timestamp, LocalDate, LocalDateTime & LocalTime added

--- src/main/java/com/inad/Factory.java
+++ src/main/java/com/inad/Factory.java
@@ -3,6 +3,9 @@ package com.inad;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -16,6 +19,24 @@ import static com.inad.Utils.randomString;
 public class Factory {
 
     private static final String LETTERS = "ABCDEFHIJKLMNOPQRSTUVWXYZ -";
+    private static final String INT = "int";
+    private static final String LONG = "long";
+    private static final String FLOAT = "float";
+    private static final String DOUBLE = "double";
+    private static final String BOOLEAN = "boolean";
+    private static final String STRING = "string";
+    private static final String INTEGER = "integer";
+    private static final String DATE = "date";
+    private static final String INSTANT = "instant";
+    private static final String TIMESTAMP = "timestamp";
+    private static final String LOCAL_DATE = "localdate";
+    private static final String LOCAL_DATE_TIME = "localdatetime";
+    private static final String LOCAL_TIME = "localtime";
+
+    private static final int SIZE_LIST = 3;
+
+    private static final String IS_COLLECTION_LOG = "isCollection";
+    private static final String CLASS_NAME_LOG = "ClassName: ";
 
     public static <T> List<T> create(final Class<T> className, final int size) {
         final List<T> list = new ArrayList<>();
@@ -52,20 +73,20 @@ public class Factory {
 
     private static <T> void setList(final T instance, final Field field)
             throws ClassNotFoundException, IllegalAccessException {
-        System.out.println("isCollection");
+        System.out.println(IS_COLLECTION_LOG);
         String cName = getGenericClassName(field.getGenericType());
         Class<?> classObject = Class.forName(cName);
-        List<?> obj = create(classObject, 3);
+        List<?> obj = create(classObject, SIZE_LIST);
         field.set(instance, obj);
     }
 
     private static <T> void setPrimitive(final T instance, final Field field) throws IllegalAccessException {
         switch (field.getType().getSimpleName()) {
-            case "int" -> field.set(instance, randomNumber(1, 10));
-            case "long" -> field.set(instance, randomNumber(100L, 1000L));
-            case "float" -> field.set(instance, randomNumber(10f, 100f));
-            case "double" -> field.set(instance, randomNumber(1.0, 10.0));
-            case "boolean" -> field.set(instance, randomBoolean());
+            case INT -> field.set(instance, randomNumber(1, 10));
+            case LONG -> field.set(instance, randomNumber(100L, 1000L));
+            case FLOAT -> field.set(instance, randomNumber(10f, 100f));
+            case DOUBLE -> field.set(instance, randomNumber(1.0, 10.0));
+            case BOOLEAN -> field.set(instance, randomBoolean());
             default -> {
             }
         }
@@ -75,14 +96,20 @@ public class Factory {
             throws IllegalAccessException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException,
             InstantiationException {
         field.trySetAccessible();
-        switch (field.getType().getSimpleName()) {
-            case "String" -> field.set(instance, randomString(10, LETTERS));
-            case "Integer" -> field.set(instance, randomNumber(1, 10));
-            case "Long" -> field.set(instance, randomNumber(100L, 1000L));
-            case "Float" -> field.set(instance, randomNumber(10f, 100f));
-            case "Double" -> field.set(instance, randomNumber(1.0, 10.0));
-            case "Boolean" -> field.set(instance, randomBoolean());
-            case "Date" -> field.set(instance, new Date());
+        final String typeName = field.getType().getSimpleName().toLowerCase();
+        switch (typeName) {
+            case STRING -> field.set(instance, randomString(10, LETTERS));
+            case INTEGER -> field.set(instance, randomNumber(1, 10));
+            case LONG -> field.set(instance, randomNumber(100L, 1000L));
+            case FLOAT -> field.set(instance, randomNumber(10f, 100f));
+            case DOUBLE -> field.set(instance, randomNumber(1.0, 10.0));
+            case BOOLEAN -> field.set(instance, randomBoolean());
+            case DATE -> field.set(instance, new Date());
+            case INSTANT -> field.set(instance, new Date().toInstant());
+            case TIMESTAMP -> field.set(instance, new Timestamp(System.currentTimeMillis()));
+            case LOCAL_DATE -> field.set(instance, LocalDate.now());
+            case LOCAL_DATE_TIME -> field.set(instance, LocalDateTime.now());
+            case LOCAL_TIME -> field.set(instance, LocalDateTime.now().toLocalTime());
             default -> {
                 String className = field.getType().getName();
                 Class<?> classObject = Class.forName(className);
@@ -95,7 +122,7 @@ public class Factory {
     private static String getGenericClassName(final Type genericType) {
         String className = genericType.getTypeName();
         className = className.substring(className.indexOf("<") + 1, className.lastIndexOf(">"));
-        System.out.println("ClassName: " + className);
+        System.out.println(CLASS_NAME_LOG.concat(className));
         return className;
     }
 
--- src/main/java/com/inad/dto/Person.java
+++ src/main/java/com/inad/dto/Person.java
@@ -3,6 +3,10 @@ package com.inad.dto;
 import lombok.Data;
 import lombok.ToString;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 
@@ -18,4 +22,8 @@ public class Person {
     private long height;
     private Date birthDate;
     private List<Phone> phones;
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private LocalDateTime localDateTime;
+    private Timestamp timestamp;
 }
